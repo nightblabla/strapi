b2fdf0e90672ea41736c0287b517ef45d0f47d12
Emre Can Eski <49168316+nightblabla@users.noreply.github.com>
2024-02-07 19:21:54 +0300

    port

--- config/env/site1/server.js
+++ config/env/site1/server.js
@@ -1,10 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('HOST_PORT_SITE1', 4338),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
 });
+
--- config/env/site2/server.js
+++ config/env/site2/server.js
@@ -1,10 +1,18 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('HOST_PORT_SITE2', 4339),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
 });
+
